d063df3111bb93ccf7c66390be29844e85cab2de
yoshi230644 <yoshi.lepus4423@gmail.com>
2023-08-21 21:18:35 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,4 +2,7 @@
   <x-slot name="header">ヘッダー２</x-slot>
 
 コンポーネントテスト２
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
