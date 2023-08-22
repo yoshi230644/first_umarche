3c9a20c26ba73e0fbe3a0d494bd1fc56432f5662
yoshi230644 <yoshi.lepus4423@gmail.com>
2023-08-22 20:00:57 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; //Eloquent エロクアント
+use Illuminate\Support\Facades\DB; //QueryBuilder
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -20,7 +23,24 @@ class OwnersController extends Controller
 
     public function index()
     {
-        dd('オーナー一覧です');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now->year;
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
+
+        // $c_test = collect([
+        //     'name' =>'てすと'
+        // ]);
+
+        // var_dump($q_first);
+
+        // dd($e_all,$q_get,$q_first,$c_test);
+        return view('admin.owners.index',
+        compact('e_all','q_get'));
     }
 
     /**
