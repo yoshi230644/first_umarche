4054b854b056f9f8254cdea826829a4c38a350c7
yoshi230644 <yoshi.lepus4423@gmail.com>
2023-08-22 19:14:54 +0900

    リソースコントローラ

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners',OwnersController::class)
+->middleware('auth:admin', 'verified');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
